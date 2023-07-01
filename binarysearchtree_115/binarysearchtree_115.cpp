5f7c577e3c5ba05bf77ed0f1d4e8e82305d92290
khaiqalsatrio <khaiqalsatrio29@gmail.com>
2023-07-01 22:16:57 +0700

    membuat if element

--- binarysearchtree_115/binarysearchtree_115.cpp
+++ binarysearchtree_115/binarysearchtree_115.cpp
@@ -44,3 +44,13 @@ public:
 			ROOT = newNode; //mark the new node as ROOT
 			return; //exit
 		}
+
+		if (element < parent->info) // if the value in the data field of the new node is less than that of the parent
+		{
+			parent->leftchild = newNode; //make the left child of the parent poin to the new node
+		}
+		else if (element > parent->info) //if the value in the data field of the new node is greater than that of the parent
+		{
+			parent->rightchild = newNode;//make the right child of the parent point to the new node
+		}
+	}
